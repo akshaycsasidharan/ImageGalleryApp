95ffb1ae6cb1af76d9fd167b2908cf57da7e7e35
AKSHAY .C.S <akshaycsasidharan@gmail.com>
2024-09-25 09:22:16 -0700

    first commit

---
+++ src/components/Gallery.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Gallery.css";
+function Gallery() {
+  return (
+    <div>
+      <h1 className='title'>Image Gallery App</h1>
+
+      <div className='image-grid'>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Gallery
