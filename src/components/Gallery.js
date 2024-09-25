27aa0393dcc29b3b9b3cdea2dd2290d13e25dca3
AKSHAY .C.S <akshaycsasidharan@gmail.com>
2024-09-25 09:44:59 -0700

    styling completed

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
 import "./Gallery.css";
 function Gallery() {
   return (
     <div>
-      <h1 className='title'>Image Gallery App</h1>
+      <h1 className="title">Image Gallery App</h1>
 
-      <div className='image-grid'>
+      <div className="image-grid">
+        <div className="image-card">
+          <div className="image-details">
 
+            <h3>Sample</h3>
+            <button>Delete</button>
+          </div>
+        </div>
       </div>
-
     </div>
   )
-}
+};
 
-export default Gallery
+export default Gallery;
