fbd50fa12804a3b3e79060c8ef1becc08566f6ec
AKSHAY .C.S <akshaycsasidharan@gmail.com>
2024-09-26 00:15:40 -0700

    successfully completed

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,21 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Gallery.css";
+
 function Gallery() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = () => {
+      const data = [
+        {
+          id: 1,
+          url: "https://picsum.photos/id/1/300/200",
+          title: "Image 1",
+        },
+        {
+          id: 2,
+          url: "https://picsum.photos/id/2/300/200",
+          title: "Image 2",
+        },
+        {
+          id: 3,
+          url: "https://picsum.photos/id/3/300/200",
+          title: "Image 3",
+        },
+        {
+          id: 4,
+          url: "https://picsum.photos/id/4/300/200",
+          title: "Image 4",
+        },
+        {
+          id: 5,
+          url: "https://picsum.photos/id/5/300/200",
+          title: "Image 5",
+        },
+      ];
+
+      setImages(data);
+    };
+
+    fetchImages();
+  }, []);
+
+  const handleDeleteImage = (imageDataId) => {
+    setImages(images.filter((image) => image.id !== imageDataId));
+  };
+
   return (
     <div>
       <h1 className="title">Image Gallery App</h1>
 
-      <div className="image-grid">
-        <div className="image-card">
-          <div className="image-details">
+      {images.length > 0 ? (
+        <div className="image-grid">
+          {images.map((imagedata) => (
+            <div className="image-card" key={imagedata.id}>
+              <img src={imagedata.url} alt="" />
 
-            <h3>Sample</h3>
-            <button>Delete</button>
-          </div>
+              <div className="image-details">
+                <h3>{imagedata.title}</h3>
+                <button onClick={() => handleDeleteImage(imagedata.id)}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-      </div>
+      ) : (
+        <p className="no-images">No Images Found</p>
+      )}
     </div>
-  )
-};
+  );
+}
 
 export default Gallery;
